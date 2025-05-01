31237330c0098e8d24d8b3e2da8b2f3c013ee9ef
Muppala Suchitha <suchitha1972@gmail.com>
2025-05-01 18:55:06 +0530

    changes commtied

--- ui/src/pages/financialapi.js
+++ ui/src/pages/financialapi.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base URL for your backend API
-const API_BASE_URL = 'https://kyc-hxkh.onrender.com/'; // Change this to your actual backend API URL
+const API_BASE_URL = 'https://kyc-hxkh.onrender.com'; // Change this to your actual backend API URL
 
 export const fetchData = async () => {
   const res = await axios.get(`${API_BASE_URL}/api/dashboard-data`);

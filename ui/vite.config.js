094a9e2a16e707a4e00b5cbb2f505680baa96d9e
Muppala Suchitha <suchitha1972@gmail.com>
2025-05-01 18:17:53 +0530

    changes commtied

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './ui',  // Ensure the root is correctly set to 'frontend' folder
+  root: './',  // Ensure the root is correctly set to 'frontend' folder
   build: {
-    outDir: './ui/build',  // Specify the output directory for the build
+    outDir: './build',  // Specify the output directory for the build
     rollupOptions: {
-      input: './ui/index.html', // Explicitly specify entry HTML file
+      input: './index.html', // Explicitly specify entry HTML file
     },
   },
 });
